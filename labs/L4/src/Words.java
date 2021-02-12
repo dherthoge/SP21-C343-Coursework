ade4c72c6611a0f36458c8bf7a4a78f99064d96d
Herthoge, Dylan M <dherthog@iu.edu>
2021-02-12 10:01:36 -0500

    Completed lab 4

--- labs/L4/src/Words.java
+++ labs/L4/src/Words.java
@@ -2,10 +2,12 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Word {
+
     private String w;
 
     Word (String w) { this.w = w; }
@@ -17,13 +19,75 @@ class Word {
      * (YOU MAY NOT USE BUILT-IN hashCode)
      */
     // TODO after testing
-    public int hashCode () { return 7; }
+    public int hashCode () {
+
+        // 9th: ~ 35 ms
+        int sum = 0;
+        for(int i = 0; i < w.length(); i++) {
+            sum += sum * 7 + w.charAt(i);
+        }
+        return sum;
+
+        // 8th: ~ 40 ms
+//        int sum = 0;
+//        for(int i = 0; i < w.length(); i++) {
+//            sum += sum * 3 + w.charAt(i);
+//        }
+//        return sum;
+
+        // 7th: ~ 32 ms
+//        int sum = 0;
+//        for(int i = 0; i < w.length(); i++) {
+//            sum += sum * 5 + w.charAt(i);
+//        }
+//        return sum;
+
+        // 6th: ~ 15966 ms
+//        int sum = 0;
+//        for(int i = 0; i < w.length(); i++) {
+//            sum *= w.charAt(i);
+//        }
+//        return sum;
+
+        // 5th: ~ 121 ms
+//        int sum = 0;
+//        for(int i = 0; i < w.length(); i++) {
+//            sum += (int) w.charAt(i) * 7;
+//        }
+//        return sum;
+
+        // 4th: ~ 563 ms
+//        int sum = 0;
+//        for(int i = 0; i < w.length(); i++) {
+//            sum += (int) w.charAt(i) % 7;
+//        }
+//        return sum;
+
+        // 4th: ~ 110 ms
+//        int sum = 0;
+//        for(int i = 0; i < w.length(); i++) {
+//            sum += (int) w.charAt(i);
+//        }
+//        return sum;
+
+        // 3rd: ~ 2405 ms
+//        return w.length() % 7;
+
+        // 2nd: ~ 1810 ms
+//        return w.length() * 2;
+
+        // 1st: ~ 1799 ms
+//        return w.length();
+
+        // Given: ~ 15904 ms
+//        return 7;
+    }
 }
 
 class Words {
+
     static String filename = "commonwords.txt";
     // from http://www.mieliestronk.com/corncob_lowercase.txt
-
     static List<String> slist;
     static List<Word> wlist;
 
@@ -37,5 +101,17 @@ class Words {
         } catch (IOException e) {
             throw new Error();
         }
+    }
+
+    public static void stringsToHashSet() {
+
+        HashSet<String> stringHashSet = new HashSet<>();
+        stringHashSet.addAll(slist);
+    }
+
+    public static void wordsToHashSet() {
+
+        HashSet<Word> wordHashSet = new HashSet<>();
+        wordHashSet.addAll(wlist);
     }
 }
--- labs/L4/test/WordsTest.java
+++ labs/L4/test/WordsTest.java
@@ -4,14 +4,43 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 
 class WordsTest {
 
     // write your own tests timing how long it takes to build a hashset with a Listof String and then a Listof Word
     // refer to Lab 3 tests if you do not remember how to time tests using methods of the Instant & Duration class
     @Test
-    void timeW () {
+    void timeTests () {
         // TODO
+
+
+        Duration d1 = timeStrings();
+        System.out.printf("Running Strings->HashSet; time in ms = %d%n",
+                d1.toMillis());
+
+
+        Duration d2 = timeWords();
+        System.out.printf("Running Words->HashSet; time in ms = %d%n",
+                d2.toMillis());
+    }
+
+    Duration timeStrings() {
+
+        Instant start = Instant.now();
+        Words.stringsToHashSet();
+        Instant end = Instant.now();
+
+
+        return Duration.between(start, end);
+    }
+
+    Duration timeWords() {
+
+        Instant start = Instant.now();
+        Words.wordsToHashSet();
+        Instant end = Instant.now();
+        return Duration.between(start, end);
     }
 
 }
